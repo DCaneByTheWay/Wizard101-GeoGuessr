2fb5e8b3c4586e5aeb748c78287c000ba839664a
Casey Connor <cconnor9000@gmail.com>
2026-04-18 01:08:48 -0500

    implemented initial score calculation

--- dest/GameController.js
+++ dest/GameController.js
@@ -41,10 +41,28 @@ const DRAG_THRESHOLD = 5; // pixels of drag
 container.addEventListener("pointerdown", (e) => {
     pointerDownPos = { x: e.clientX, y: e.clientY };
 });
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') {
+        submitGuess();
+        e.preventDefault();
+    }
+});
 const MARKER_SRC = "./Images/Markers/(Icon)_Place_Mark.png";
 const ANSWER_MARKER_SRC = "./Images/Markers/(Icon)_Quests.png";
 let currentMarker = null;
 const submitAnswerButton = document.getElementById("submit-guess-button");
+const scoreDisplay = document.getElementById("score-display");
+const scoreBreakdown = document.getElementById("score-breakdown");
+let currentGuessImage = null;
+// max score for a perfect guess
+const MAX_SCORE = 5000;
+// weights of each of the components of a guess (out of 1)
+const WORLD_VALUE_WEIGHT = 0.1;
+const AREA_VALUE_WEIGHT = 0.15;
+// score values 
+const WORLD_SCORE = MAX_SCORE * WORLD_VALUE_WEIGHT;
+const AREA_SCORE = MAX_SCORE * AREA_VALUE_WEIGHT;
+const MAXIMUM_DISTANCE_SCORE = MAX_SCORE - (WORLD_SCORE + AREA_SCORE);
 export function setBackgroundImage(src) {
     if (imageElement) {
         imageWrapper?.classList.add("spinner-container");
@@ -64,10 +82,66 @@ export function submitGuess() {
     if (!currentMarker)
         return;
     console.log("submitted!");
-    const answerMark = { key: '', xPercent: 0.5, yPercent: 0.5 };
+    const answerMark = currentGuessImage?.solutionMarker;
     const score = getCalculatedScore(currentMarker, answerMark);
+    console.log(`your score: ${score.toFixed(0)}/${MAX_SCORE}`);
+    scoreDisplay.textContent = `You Scored ${score.toFixed(0).toString()}/${MAX_SCORE}!`;
+    //scoreBreakdown!.textContent = `Correct World: `; //TODO: make breakdown
+}
+/** Takes two marks and returns distance */
+function getMarkDistance(mark1, mark2) {
+    const dx = mark1.xPercent - mark2.xPercent;
+    const dy = mark1.yPercent - mark2.yPercent;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance;
 }
 export function getCalculatedScore(guessMark, answerMark) {
+    let score = 0;
+    // if guess marker is at spiral level
+    if (guessMark.key === "spiral") {
+        // TODO: implement logic to find if marker is on world icon
+    }
+    // if guess marker is at world level
+    else if (guessMark.key.includes("world")) {
+        // TODO: finish deciding how to calculate score at this level
+        // prob either place marker 50% away from answer marker, or place marker in center of area to calculate distance
+    }
+    // if guess marker is at area level
+    else if (guessMark.key.includes("area")) {
+        const [, guessWorld, guessArea] = guessMark.key.split(":");
+        const [, answerWorld, answerArea] = answerMark.key.split(":");
+        // if world incorrect
+        if (guessWorld !== answerWorld) {
+            console.log('incorrect world');
+            console.log(`guess: '${guessWorld}', answer: '${answerWorld}'`);
+            score = 0;
+        }
+        // if world correct and area incorrect
+        else if (guessArea !== answerArea) {
+            console.log('correct world, incorrect area');
+            console.log(`guess: '${guessArea}', answer: '${answerArea}'`);
+            score = WORLD_SCORE;
+        }
+        // if world correct and area correct
+        else if (guessArea === answerArea) {
+            console.log('correct world, correct area');
+            const distance = getMarkDistance(guessMark, answerMark);
+            score = WORLD_SCORE + AREA_SCORE + MAXIMUM_DISTANCE_SCORE * Math.E ** (-6.7 * distance / currentGuessImage?.mapSize);
+        }
+        else {
+            console.log('how did we get here');
+            console.log(`guess: ${guessMark.key}, answer: ${guessMark.key}`);
+        }
+        const distance = getMarkDistance(guessMark, answerMark);
+        console.log(`guess: ${guessMark.xPercent.toFixed(1)}, ${guessMark.yPercent.toFixed(1)}`);
+        console.log(`answer: ${answerMark.xPercent.toFixed(1)}, ${answerMark.yPercent.toFixed(1)}`);
+        console.log(`distance from answer: ${distance.toFixed(1)}`);
+        console.log(`score: ${score}`);
+    }
+    else {
+        console.log('how did we get here');
+        console.log(`key: ${guessMark.key}`);
+    }
     /*
     Scoring System:
     out of 100%
@@ -121,14 +195,16 @@ export function getCalculatedScore(guessMark, answerMark) {
     print(score)
 
     */
-    return 1;
+    return score;
 }
 export function getRandomImagePath() {
-    //const wizCityImageList = Object.values(guessImages).flat().filter(img => img.difficulty === Difficulty.EASY);
-    const worldName = "Dragonspyre";
-    const iamgeList = Object.values(guessImages).flat().filter(img => img.imgSrc.includes(worldName));
-    const imgSrc = iamgeList[Math.floor(Math.random() * iamgeList.length)]?.imgSrc;
+    //const worldName = "Dragonspyre";
+    //const imageList = Object.values(guessImages).flat().filter(img => img.imgSrc.includes(worldName));
+    const imageList = Object.values(guessImages).flat();
+    const randomGuessImage = imageList[Math.floor(Math.random() * imageList.length)];
+    const imgSrc = randomGuessImage.imgSrc;
     console.log(`current img: ${imgSrc}`);
+    currentGuessImage = randomGuessImage;
     return imgSrc;
 }
 export function placeMarker(e) {
@@ -169,12 +245,12 @@ export function placeMarker(e) {
         marker.style.left = xPercent + "%";
         marker.style.top = yPercent + "%";
         spiralContent.appendChild(marker);
-        // update current mark
-        currentMarker = { key: getLevelKey(), xPercent, yPercent };
         // update submit button text
         submitAnswerButton.textContent = "Submit your Guess!";
         submitAnswerButton.style.backgroundColor = 'rgb(30, 2, 90)';
     }
+    // update current mark
+    currentMarker = { key: getLevelKey(), xPercent, yPercent };
     console.log(`x:${xPercent.toFixed(1)}\ny:${yPercent.toFixed(1)}`);
 }
 export function saveMarker() {
--- src/GameController.ts
+++ src/GameController.ts
@@ -1,5 +1,5 @@
 import { getLevelKey, getTransform } from "./ZoomController.js";
-import { Difficulty, guessImages } from "./ImageData.js";
+import { Difficulty, guessImages, GuessImage } from "./ImageData.js";
 
 const images = [
     "WizardCity1",
@@ -47,12 +47,35 @@ container!.addEventListener("pointerdown", (e) => {
     pointerDownPos = {x: e.clientX, y: e.clientY};
 });
 
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Space') {
+    submitGuess();
+    e.preventDefault(); 
+  }
+});
+
 const MARKER_SRC = "./Images/Markers/(Icon)_Place_Mark.png";
 const ANSWER_MARKER_SRC = "./Images/Markers/(Icon)_Quests.png";
 
 let currentMarker: {key: string, xPercent: number, yPercent: number} | null = null;
 
-const submitAnswerButton = document.getElementById("submit-guess-button")
+const submitAnswerButton = document.getElementById("submit-guess-button");
+const scoreDisplay = document.getElementById("score-display");
+const scoreBreakdown = document.getElementById("score-breakdown");
+
+let currentGuessImage: GuessImage | null = null;
+
+// max score for a perfect guess
+const MAX_SCORE = 5000;
+
+// weights of each of the components of a guess (out of 1)
+const WORLD_VALUE_WEIGHT = 0.1;
+const AREA_VALUE_WEIGHT = 0.15;
+
+// score values 
+const WORLD_SCORE = MAX_SCORE * WORLD_VALUE_WEIGHT;
+const AREA_SCORE = MAX_SCORE * AREA_VALUE_WEIGHT;
+const MAXIMUM_DISTANCE_SCORE = MAX_SCORE - (WORLD_SCORE + AREA_SCORE);
 
 export function setBackgroundImage(src: string): void {
     if (imageElement) {
@@ -75,14 +98,85 @@ export function submitGuess(): void {
     if (!currentMarker) return
 
     console.log("submitted!");
-    const answerMark = {key: '', xPercent: 0.5, yPercent: 0.5};
+    const answerMark = currentGuessImage?.solutionMarker!;
     const score = getCalculatedScore(currentMarker, answerMark);
+    console.log(`your score: ${score.toFixed(0)}/${MAX_SCORE}`);
+
+    scoreDisplay!.textContent = `You Scored ${score.toFixed(0).toString()}/${MAX_SCORE}!`;
+    //scoreBreakdown!.textContent = `Correct World: `; //TODO: make breakdown
+}
+
+/** Takes two marks and returns distance */
+function getMarkDistance(
+    mark1: {key: string, xPercent: number, yPercent: number}, 
+    mark2: {key: string, xPercent: number, yPercent: number}
+): number {
+    
+    const dx = mark1.xPercent - mark2.xPercent;
+    const dy = mark1.yPercent - mark2.yPercent;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance
 }
 
 export function getCalculatedScore(
     guessMark: {key: string, xPercent: number, yPercent: number},
     answerMark: {key: string, xPercent: number, yPercent: number}
 ): number {
+    
+    let score = 0;
+
+    // if guess marker is at spiral level
+    if (guessMark.key === "spiral") {
+        // TODO: implement logic to find if marker is on world icon
+    }
+    // if guess marker is at world level
+    else if (guessMark.key.includes("world")) {
+        // TODO: finish deciding how to calculate score at this level
+        // prob either place marker 50% away from answer marker, or place marker in center of area to calculate distance
+
+    }
+    // if guess marker is at area level
+    else if (guessMark.key.includes("area")) {
+        
+        const [, guessWorld, guessArea] = guessMark.key.split(":");
+        const [, answerWorld, answerArea] = answerMark.key.split(":");
+
+
+        // if world incorrect
+        if (guessWorld !== answerWorld) {
+            console.log('incorrect world')
+            console.log(`guess: '${guessWorld}', answer: '${answerWorld}'`)
+            score = 0;
+        }
+        // if world correct and area incorrect
+        else if (guessArea !== answerArea) {
+            console.log('correct world, incorrect area')
+            console.log(`guess: '${guessArea}', answer: '${answerArea}'`)
+            score = WORLD_SCORE;
+        }
+        // if world correct and area correct
+        else if (guessArea === answerArea) {
+            console.log('correct world, correct area')
+            const distance = getMarkDistance(guessMark, answerMark);
+            score = WORLD_SCORE + AREA_SCORE + MAXIMUM_DISTANCE_SCORE * Math.E ** (-6.7 * distance/currentGuessImage?.mapSize!)
+        }
+        else {
+        console.log('how did we get here');
+        console.log(`guess: ${guessMark.key}, answer: ${guessMark.key}`);
+        }
+        
+        
+        const distance = getMarkDistance(guessMark, answerMark);
+        console.log(`guess: ${guessMark.xPercent.toFixed(1)}, ${guessMark.yPercent.toFixed(1)}`)
+        console.log(`answer: ${answerMark.xPercent.toFixed(1)}, ${answerMark.yPercent.toFixed(1)}`)
+        console.log(`distance from answer: ${distance.toFixed(1)}`)
+        console.log(`score: ${score}`);
+    }
+    else {
+        console.log('how did we get here');
+        console.log(`key: ${guessMark.key}`);
+    }
+
 
     /*
     Scoring System:
@@ -138,16 +232,17 @@ export function getCalculatedScore(
 
     */
 
-
-    return 1;
+    return score;
 }
 
 export function getRandomImagePath(): string {
-    //const wizCityImageList = Object.values(guessImages).flat().filter(img => img.difficulty === Difficulty.EASY);
-    const worldName = "Dragonspyre";
-    const iamgeList = Object.values(guessImages).flat().filter(img => img.imgSrc.includes(worldName));
-    const imgSrc = iamgeList[Math.floor(Math.random() * iamgeList.length)]?.imgSrc!
+    //const worldName = "Dragonspyre";
+    //const imageList = Object.values(guessImages).flat().filter(img => img.imgSrc.includes(worldName));
+    const imageList = Object.values(guessImages).flat();
+    const randomGuessImage = imageList[Math.floor(Math.random() * imageList.length)]!;
+    const imgSrc = randomGuessImage.imgSrc!;
     console.log(`current img: ${imgSrc}`)
+    currentGuessImage = randomGuessImage;
     return imgSrc;
 
 }
@@ -200,14 +295,14 @@ export function placeMarker(e: PointerEvent): void {
     
         spiralContent!.appendChild(marker);
 
-        // update current mark
-        currentMarker = {key: getLevelKey(), xPercent, yPercent};
-
+        
         // update submit button text
         submitAnswerButton!.textContent = "Submit your Guess!";
         submitAnswerButton!.style.backgroundColor = 'rgb(30, 2, 90)';
-
+        
     }
+    // update current mark
+    currentMarker = {key: getLevelKey(), xPercent, yPercent};
     console.log(`x:${xPercent.toFixed(1)}\ny:${yPercent.toFixed(1)}`);
 }
 
